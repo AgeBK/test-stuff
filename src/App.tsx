1e1bb210424374c4e2b6764a4cf942e57d1a3925
AgeBK <age24@hotmail.com>
2024-02-17 14:50:06 +1100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,16 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <img src="/assets/barrel.png" alt="test" />
+      <img src="./assets/barrel.png" alt="test" />
+      <img src="../assets/barrel.png" alt="test" />
       <img src="src/assets/barrel.png" alt="test" />
+      <img src="/src/assets/barrel.png" alt="test" />
+      <img src="./src/assets/barrel.png" alt="test" />
+      <img src="../src/assets/barrel.png" alt="test" />
+      <img src="/../src/assets/barrel.png" alt="test" />
+      <img src="./../src/assets/barrel.png" alt="test" />
+      <img src="../../src/assets/barrel.png" alt="test" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
